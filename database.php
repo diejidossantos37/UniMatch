bb569380c166d1ab5d95d48c98a42ab7ddb6f317
Dieji dos Santos <diejidossantos37@gmail.com>
2026-05-14 16:05:43 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,1 +1,22 @@
+<?php
 
+$host = "localhost";
+$db   = "unimatch";
+$user = "root";
+$pass = "";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+
+    die("Erro BD: " . $e->getMessage());
+}
+?>
